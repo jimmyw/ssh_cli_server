deb26cda5658bf1c8442385cbfa6d470ac464890
Jimmy Wennlund <jimmy@wennlund.nu>
2025-11-11 22:53:27 +0100

    Initial add

---
+++ include/ssh_server.h
@@ -0,0 +1,25 @@
+#include <stdint.h>
+#include "esp_err.h"
+
+
+typedef void (*ssh_shell_func_t)(void *ctx);
+
+typedef struct {
+    const char *bindaddr;
+    const char *port;
+    const char *debug_level;
+    const char *username;
+    const char *host_key;  // SSH host private key (PEM format)
+#if CONFIG_EXAMPLE_ALLOW_PASSWORD_AUTH
+    const char *password;
+#endif
+#if CONFIG_EXAMPLE_ALLOW_PUBLICKEY_AUTH
+    const char *allowed_pubkeys;
+#endif
+    ssh_shell_func_t shell_func;
+    void *shell_func_ctx;
+    uint32_t shell_task_size;
+    bool shell_task_kill_on_disconnect; // Whether to kill the shell task when the SSH channel is closed, can be dangerous.
+} ssh_server_config_t;
+
+esp_err_t ssh_server_start(ssh_server_config_t *config);
